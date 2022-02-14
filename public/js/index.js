1c1a726646add9f64a91e78e6e90f17ae14b4860
Sally Resch <Sengangarn@Sallys-MacBook-Pro.local>
2022-02-14 10:00:33 +0100

    try make table in javascript

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+var anElement = document.getElementById("app")
+anElement.innerHTML = "Hello World"
+anElement.outerHTML = "Hello World"
+
+var app = document.getElementById("app")
+app.appendChild(anElement)
+  < button onClicks = "age()" > Click me</button >
+
+
+    setTimeout(age, 3000)
+
+function age() {
+  console.log("aging game")
+  setTimeout(age, 3000)
+}
