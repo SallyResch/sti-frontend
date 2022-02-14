4eff49bf955a9219fb7a7c4c8c62fd65e0f48abc
Sally Resch <Sengangarn@Sallys-MacBook-Pro.local>
2022-02-14 13:19:48 +0100

    Made a button click and tablerows

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,37 @@ var anElement = document.getElementById("app")
 anElement.innerHTML = "<button onClick='buttonClick()'>Click me</button>";
 
 function buttonClick() {
+  var x = document.createElement("TABLE");
+  x.setAttribute("id", "myTable");
+  document.body.appendChild(x);
+
+  var tableRow = document.createElement("TR");
+  tableRow.setAttribute("id", "myTr");
+  document.getElementById("myTable").appendChild(tableRow);
+
+  var td = document.createElement("TD");
+  var tCell = document.createTextNode("cell");
+  td.appendChild(tCell);
+  document.getElementById("myTr").appendChild(td);
+}
+/*var app = document.getElementById("app")
+app.appendChild(anElement)
+
+function buttonClick() {
   anElement.outerHTML = "<strong>Sally</strong>"
 }
 
-var app = document.getElementById("app")
-app.appendChild(anElement)
+function createRow() {
+  var allow = document.createElement("tr")
+  allow.appendChild(createCell())
+}
+
+function createCell() {
+  var aCell = document.createElement("td")
+  return aCell;
+}*/
+
+
 
 
 /*setTimeout(age, 3000)
