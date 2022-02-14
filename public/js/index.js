385e147cfc86ce80af7ccb342132cbe0fb48d2f6
Sally Resch <Sengangarn@Sallys-MacBook-Pro.local>
2022-02-14 10:56:27 +0100

    fix buttonClick function in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,19 @@
 console.log("Hello World");
 
 var anElement = document.getElementById("app")
-//anElement.innerHTML = "Hello World"
-//anElement.outerHTML = "Hello World"
+anElement.innerHTML = "<button onClick='buttonClick()'>Click me</button>";
+
+function buttonClick() {
+  anElement.outerHTML = "<strong>Sally</strong>"
+}
 
 var app = document.getElementById("app")
 app.appendChild(anElement)
 
 
-setTimeout(age, 3000)
+/*setTimeout(age, 3000)
 
 function age() {
   console.log("aging game")
   setTimeout(age, 3000)
-}
+}*/
