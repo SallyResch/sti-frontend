de27a6de723a6429ad4feb63d4e2eb87fc81b453
Sally Resch <Sengangarn@Sallys-MacBook-Pro.local>
2022-02-16 13:51:01 +0100

    Ajax things 16 feb

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 function age(params) {
   var xhr = new XMLHttpRequest()
   xhr.open("GET", "/js/data.json")
@@ -32,41 +30,3 @@ function createCell(content) {
   aCell.innerHTML = content
   return aCell;
 }
-
-/*
-//Mikaels kod för att skapa en tabell med rader och en cell i varje ny rad
-var ettElement = document.getElementById("app")
-
-var aTable = document.createElement("table")
-aTable.appendChild(createRow())
-
-ettElement.appendChild(aTable)
-
-function buttonClick2() {
-  aTable.appendChild(createRow())
-}
-
-function createRow() {
-  var aRow = document.createElement("tr")
-  aRow.appendChild(createCell())
-  return aRow
-}
-
-function createCell() {
-  var aCell = document.createElement("td")
-  return aCell;
-}*/
-
-/*
-//egen kod
-function buttonClick() {
-  anElement.outerHTML = "<strong>Sally</strong>"
-}
-
-//Hur man sätter igång ett spel
-setTimeout(age, 3000)
-function age() {
-  console.log("aging game")
-  setTimeout(age, 3000)
-}*/
-
