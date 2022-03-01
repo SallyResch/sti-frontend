d06aa72a8b968aaa07905a85e1e889e63f689397
Sally Resch <Sengangarn@Sallys-MacBook-Pro.local>
2022-03-01 13:00:48 +0100

    try to get fortnitetracker work

--- public/js/fortnite.js
+++ public/js/fortnite.js
@@ -1,4 +1,4 @@
-const res = require("express/lib/response");
+const req = require("express/lib/response");
 
 let submitBtn = document.getElementById('submit');
 let gamertagInput = document.getElementById('gamertag');
@@ -6,7 +6,7 @@ let platformInput = document.getElementById('platform');
 let result = document.querySelector('.result');
 
 const fetchPlayers = async (gamertag, platform) => {
-  const api_call = await fetch(`https://api.fortnitetracker.com/v1/profile/${platform}/${epic - nickname}`, {
+  const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/profile/${platform}/${gamertag}`, {
     headers: {
       'TRN-Api-Key': '14df28ad- d605 - 40b7- 87f8 - 5596a4cb0140'
     }
@@ -43,9 +43,19 @@ const showData = () => {
       </div>
       ;
     result.insertAdjacentHTML('beforeend', markup);
-  });
+  })
+    .catch(err => console.log(err));
+};
+const clearField = () => {
+  gamertagInput.value = '';
+  platformInput.value = 'Choose Platform';
 };
 
+const clearPlayer = () => {
+  result.innerHTML = '';
+}
 submitBtn.addEventListener('click', function () {
   showData();
+  clearField();
+  clearPlayer();
 });
