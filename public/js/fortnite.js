cf5571eb222972118fc084f38e40431b7dfd5144
Sally Resch <Sengangarn@Sallys-MacBook-Pro.local>
2022-03-01 10:51:32 +0100

    fortnite html,css and js writen

---
+++ public/js/fortnite.js
@@ -0,0 +1,51 @@
+const res = require("express/lib/response");
+
+let submitBtn = document.getElementById('submit');
+let gamertagInput = document.getElementById('gamertag');
+let platformInput = document.getElementById('platform');
+let result = document.querySelector('.result');
+
+const fetchPlayers = async (gamertag, platform) => {
+  const api_call = await fetch(`https://api.fortnitetracker.com/v1/profile/${platform}/${epic - nickname}`, {
+    headers: {
+      'TRN-Api-Key': '14df28ad- d605 - 40b7- 87f8 - 5596a4cb0140'
+    }
+  });
+  const data = await api_call.json();
+  return { data }
+};
+
+const showData = () => {
+  fetchPlayers(gamertagInput.value, platformInput.value).then((res) => {
+    const markup =
+      <div class="stats text-center">
+        <h1>${res.data.epicUserHandle} (${res.data.platformNameLong})</h1>
+        <div class="row">
+          <div class="col-md-4 mb-3">
+            <div class="stat">
+              <h5>${res.data.lifeTimeStats[8].value}</h5>
+              <h6>Wins</h6>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="stat">
+              <h5>${res.data.lifeTimeStats[10].value}</h5>
+              <h6>Kills</h6>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="stat">
+              <h5>${res.data.lifeTimeStats[7].value}</h5>
+              <h6>Matches played</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+      ;
+    result.insertAdjacentHTML('beforeend', markup);
+  });
+};
+
+submitBtn.addEventListener('click', function () {
+  showData();
+});
